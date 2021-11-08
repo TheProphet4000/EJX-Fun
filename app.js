20c92930f862b0db3a83573c22823ff443896da5
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-11-08 11:57:07 +0100

    Done

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get("/", (req,res) => {
     res.sendFile("index.html", {root: path.join(__dirname, "./")});
 });
+
+app.post("/", urlencodedParser, (req,res) => {
+    console.log(req.body);
+});
+
 
 app.use("/", (req,res) => {
     console.log("Heyyy, i got a log working");
