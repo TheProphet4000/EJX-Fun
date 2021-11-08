793e7affc23da9115372bf0611f252df674a3357
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-11-08 13:16:14 +0100

    brain smashed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-//sender alt hvad brugeren har skrevet
+//sender alt hvad brugeren har skrevet. Dette er end points
 app.get("/", (req,res) => {
     res.sendFile("index.html", {root: path.join(__dirname, "./")});
 });
@@ -14,7 +14,12 @@ app.get("/", (req,res) => {
 app.get("/sms1", (req,res) => {
     res.sendFile("sms1.html", {root: path.join(__dirname, "./")});
 });
+
 app.get("/sms2", (req,res) => {
+    res.sendFile("sms2.html", {root: path.join(__dirname, "./")});
+});
+
+app.get("/rtype", (req,res) => {
     res.sendFile("sms2.html", {root: path.join(__dirname, "./")});
 });
 
@@ -30,9 +35,21 @@ app.post("/sms2", urlencodedParser, (req,res) => {
     console.log(req.body);
 });
 
+app.post("/rtype", urlencodedParser, (req,res) => {
+    console.log(res.body);
+});
+
 //smider en log ud når root dir, bliver tilgået.. her kan man auth brugeren
 app.use("/", (req,res) => {
     console.log("Heyyy, i got a log working");
+        m = req.method;
+        console.log(m);
+        res.send('ok');
+});
+
+app.use("/rtype", (req,res) => {
+        console.log(req.method);
+        res.send('ok got you');
 });
 
 //starter serveren
